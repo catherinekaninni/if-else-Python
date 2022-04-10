5c328892f2dea4d5209f5b65ec4b64cffebafac7
cathernekaninni <catherinekaninni@gmail.com>
2022-04-10 15:58:18 +0300

    first commit

---
+++ if.py
@@ -0,0 +1,16 @@
+print("welcome to the rollcoaster!")
+height= int(input("what's your height in cm?"))
+
+if height >= 120:
+    print("You can override the rollercoster")
+else:
+    print("You have to grow taller before you can ride") 
+
+    print("Welcome to the party") 
+    age= int(input("What is your age?"))  
+
+    if age <= 6:
+        print("Serve a glass of milk")
+    else:
+        print("Serve cocacola")
+        
